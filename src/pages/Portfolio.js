f8b5c7e3845c4e05903a64d1a0ebd599ff4f5ff8
Pete VS <pete_vs88@me.com>
2021-10-12 21:46:41 -0600

    moved recurringBuyList to portfolioReducer

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,5 +1,5 @@
 import { Button, Table, TableRow, TableCell, TableBody} from '@mui/material'
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { GlobalContext } from 'state/contexts/GlobalContext'
 import { recurringBuy } from 'utils/recurringBuy'
 import RecurringBuyForm from 'components/RecurringBuyForm'
@@ -11,6 +11,7 @@ import EditPortfolioForm from 'components/EditPortfolioForm'
 import MyTableRow from 'styledComponents/MyTableRow'
 import TransactionForm from 'components/TransactionForm'
 import Chart from 'react-apexcharts'
+import { db } from 'firebase'
 
 const Portfolio = () => {
 
@@ -32,31 +33,16 @@ const Portfolio = () => {
     
     const handleClose = () => setOpen(false);
 
-  
+    console.log(state.portfolio.recurringBuyList()[id])
+
     //If No Portfolio Data...
-    if(state.portfolio.portfolioList.length < 1){
+    if(state.portfolio.portfolioList.length < 1 ){
         return(
             <>
                 Add Details...
             </>
         )
     }
-
-    //Create Recurring Buy List
-        const recurringBuyList = []
-
-        if('recurringBuys' in details){
-            for (const key in details.recurringBuys){
-                const buy = details.recurringBuys[key]
-                recurringBuyList.push({
-                    id: key,
-                    purchaseAmount: buy['purchaseAmount'],
-                    startDate: buy['startDate'],
-                    endDate: buy['endDate'],
-                    condition: buy['condition']
-                })
-            }
-        }    
 
 
     // Create Transaction List
@@ -75,150 +61,151 @@ const Portfolio = () => {
         }
     }
 
+    console.log(state)
+
     //Create All Transactions
     
-    let allTransactions = []
+    // let allTransactions = []
 
 
     //Go through each recurring buy and add to all Transactions
-    for (const key in details.recurringBuys) {
+    // for (const key in details.recurringBuys) {
 
-        const item = details.recurringBuys[key]
+    //     const item = details.recurringBuys[key]
         
-        const buyList = recurringBuy(
-            item.purchaseAmount,
-            item.startDate,
-            item.endDate,
-            state.portfolio.historicalData
-        )
+    //     const buyList = recurringBuy(
+    //         item.purchaseAmount,
+    //         item.startDate,
+    //         item.endDate,
+    //         state.portfolio.historicalData
+    //     )
 
-        allTransactions = [...allTransactions, ...buyList]
+    //     allTransactions = [...allTransactions, ...buyList]
     
-    }
+    // }
 
     //Get Price on the Date of Each One-Off Transaction and Then Add to All Transactions
 
-    const histData = state.portfolio.historicalDataObj()
+    // const histData = state.portfolio.historicalDataObj()
 
-    const transactionsWithPrice = transactions.map(item => {
-        return {
-            date: item.date,
-            amount: Number(item.amount),
-            price: histData[item.date]
-        }
-    })
+    // const transactionsWithPrice = transactions.map(item => {
+    //     return {
+    //         date: item.date,
+    //         amount: Number(item.amount),
+    //         price: histData[item.date]
+    //     }
+    // })
 
 
 
-    allTransactions = [...allTransactions, ...transactionsWithPrice]
+    // allTransactions = [...allTransactions, ...transactionsWithPrice]
 
     //Sort by Date
-    allTransactions = allTransactions.sort(function(a,b){
-        return new Date(a.date).getTime() - new Date(b.date).getTime()
-    })
+    // allTransactions = allTransactions.sort(function(a,b){
+    //     return new Date(a.date).getTime() - new Date(b.date).getTime()
+    // })
 
 
     //Get Running Balance and Other Calculations for All Transactions
 
-    let runningBal = 0
-    let totalInvested = 0
+    // let runningBal = 0
+    // let totalInvested = 0
 
-    const calculatedTransactions = allTransactions.map(item => {
+    // const calculatedTransactions = allTransactions.map(item => {
 
-        totalInvested = Number(totalInvested) + Number(item.amount)
-        const bitcoinAdded = Number((item.amount / item.price))
-        runningBal = runningBal + bitcoinAdded
-        const value = (item.price * runningBal).toFixed(2);
-        const profit = value - totalInvested;
-        const roi = ((value - totalInvested) / totalInvested) * 100;
+    //     totalInvested = Number(totalInvested) + Number(item.amount)
+    //     const bitcoinAdded = Number((item.amount / item.price))
+    //     runningBal = runningBal + bitcoinAdded
+    //     const value = (item.price * runningBal).toFixed(2);
+    //     const profit = value - totalInvested;
+    //     const roi = ((value - totalInvested) / totalInvested) * 100;
 
 
-        return {
-            date: item.date,
-            price: item.price,
-            amount: item.amount,
-            totalInvested: totalInvested,
-            runningBal: runningBal,
-            value: value,
-            profit: profit,
-            roi: roi
+    //     return {
+    //         date: item.date,
+    //         price: item.price,
+    //         amount: item.amount,
+    //         totalInvested: totalInvested,
+    //         runningBal: runningBal,
+    //         value: value,
+    //         profit: profit,
+    //         roi: roi
 
-        }
+    //     }
 
-    })
+    // })
 
-    console.log(calculatedTransactions)
 
     //CHART SERIES & OPTIONS
 
-    const options = {
-        chart: {
-          toolbar: {
-            show: false,
-            // tools: {
-            //     download: false,
-            // }
-          },
-        },
-        dataLabels: {
-          enabled: false,
-        },
-        yaxis: {
-          labels: {
-            show: false,
-            // formatter: function (value) {
-            //   return "$" + numberWithCommas(value);
-            // },
-            // style: {
-            //   colors: ["#fff"],
-            // },
-          },
-          // opposite: true,
-        },
-        xaxis: {
-          type: "datetime",
-          categories: calculatedTransactions.map((item) => {
-            return item.date;
-          }).reverse(),
-          labels: {
-            style: {
-              colors: "#fff",
-            },
-          },
-        },
-        colors: ["#2E99FE", "#FF2F30"],
-        tooltip: {
-          x: {
-            format: "dd MMM HH:mm",
-          },
-          theme: "dark",
-        },
-        annotations: {
-        },
-        grid: {
-          yaxis: {
-            lines: {
-              show: false,
-            },
-          },
-        },
-        legend: {
-          position: "top",
-          horizontalAlign: "right",
-          labels: {
-            colors: "#fff",
-          },
-        },
-      };
+    // const options = {
+    //     chart: {
+    //       toolbar: {
+    //         show: false,
+    //         // tools: {
+    //         //     download: false,
+    //         // }
+    //       },
+    //     },
+    //     dataLabels: {
+    //       enabled: false,
+    //     },
+    //     yaxis: {
+    //       labels: {
+    //         show: false,
+    //         // formatter: function (value) {
+    //         //   return "$" + numberWithCommas(value);
+    //         // },
+    //         // style: {
+    //         //   colors: ["#fff"],
+    //         // },
+    //       },
+    //       // opposite: true,
+    //     },
+    //     xaxis: {
+    //       type: "datetime",
+    //       categories: dbTransactions.map((item) => {
+    //         return item.date;
+    //       }).reverse(),
+    //       labels: {
+    //         style: {
+    //           colors: "#fff",
+    //         },
+    //       },
+    //     },
+    //     colors: ["#2E99FE", "#FF2F30"],
+    //     tooltip: {
+    //       x: {
+    //         format: "dd MMM HH:mm",
+    //       },
+    //       theme: "dark",
+    //     },
+    //     annotations: {
+    //     },
+    //     grid: {
+    //       yaxis: {
+    //         lines: {
+    //           show: false,
+    //         },
+    //       },
+    //     },
+    //     legend: {
+    //       position: "top",
+    //       horizontalAlign: "right",
+    //       labels: {
+    //         colors: "#fff",
+    //       },
+    //     },
+    //   };
     
-      const series = [
-        {
-          name: `Portfolio Value (${state.settings.currency})`,
-          data: calculatedTransactions.map((item) => {
-            return item.value;
-          }).reverse(),
-        },
-      ];
+    //   const series = [
+    //     {
+    //       name: `Portfolio Value (${state.settings.currency})`,
+    //       data: dbTransactions.map((item) => {
+    //         return item.value;
+    //       }).reverse(),
+    //     },
+    //   ];
 
 
   
@@ -251,7 +238,7 @@ const Portfolio = () => {
             >
                 Edit
             </Button>
-            <ChartWrapper>
+            {/* <ChartWrapper>
                 <Chart
                         series={series}
                         options={options}
@@ -259,7 +246,7 @@ const Portfolio = () => {
                         width='100%'
                         height="400px"
                     />
-            </ChartWrapper>
+            </ChartWrapper> */}
 
             <Box>
                 <HeaderRow>
@@ -289,7 +276,7 @@ const Portfolio = () => {
                         </TableRow>
                     </MyTableHead>
                     <TableBody>
-                    {recurringBuyList.map((row) => (
+                    {state.portfolio.recurringBuyList()[id].map((row) => (
                             <MyTableRow
                                 key={row.id}
                             >
--- src/state/reducers/portfolioReducer.js
+++ src/state/reducers/portfolioReducer.js
@@ -22,6 +22,54 @@ export const initialPortfolioState = {
       })
 
     return data
+  },
+  recurringBuyList: function(){
+
+    let recurringBuyList = {}
+
+    //If portfolio object not empy
+    if(this.portfolioObj){
+
+
+      //Loop through each portfolio
+      for (const portfolioID in this.portfolioObj){
+
+        //Current Portfolio
+        const current = this.portfolioObj[portfolioID]
+
+        //Initiate empty recurring buy list for current portfolio
+        const currentRecurringBuyList = []
+
+        //If there is a recurring buys object in the current portfolio
+        if('recurringBuys' in current){
+
+          //Loop through each recurring buy
+          for (const buyID in current.recurringBuys){
+            const currentBuy = current.recurringBuys[buyID]
+
+            //Push the details to the current portfolio recurring buy list
+            currentRecurringBuyList.push({
+              id: buyID,
+              purchaseAmount: currentBuy['purchaseAmount'],
+              startDate: currentBuy['startDate'],
+              endDate: currentBuy['endDate'],
+              condition: currentBuy['condition']
+            })
+          }
+        }
+
+        //Take the current portfolio ID and the currentRecurring Buy List and add to Parent Recurring Buy List
+
+        recurringBuyList = {
+          ...recurringBuyList,
+          [portfolioID]: currentRecurringBuyList
+        }
+      }
+
+    }
+
+    return recurringBuyList
+
   }
 };
 
