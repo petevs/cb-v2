939bd3692e54f1f120ba0b485495f11ba76e8039
Pete VS <pete_vs88@me.com>
2021-10-25 12:49:38 -0600

    added csv parsing to add data to array

--- src/components/UploadCsv.js
+++ src/components/UploadCsv.js
@@ -45,10 +45,30 @@ const UploadCsv = ({ portfolioId }) => {
         setUploaded(true)
     }
 
+
+    const csvToArray = (str, delimiter = ',') => {
+        const headers = str.slice(0, str.indexOf("\n")).split(delimiter)
+
+        const rows = str.slice(str.indexOf("\n") + 1).split("\n")
+
+        const arr = rows.map((row) => {
+            const values = row.split(delimiter)
+            const el = headers.reduce((object, header, index) => {
+                object[header] = values[index]
+                return object
+            }, {})
+            return el
+        })
+
+        return arr
+    }
+
     useEffect(() => {
         axios.get(downloadURL)
-            .then(({data}) => console.log(data))
+            .then(({data}) => console.log(csvToArray(data)))
     }, [uploaded])
+    
+
 
 
     if(!uploaded){
