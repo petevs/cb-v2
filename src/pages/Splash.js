010008cc833421820a3a01ff0a6947e2b7e4990d
Pete VS <pete_vs88@me.com>
2021-10-28 11:08:30 -0600

    added splash welcome popup

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import FormModal from 'components/FormModal'
+import { Button } from '@mui/material'
+import { FaUserSecret } from 'react-icons/fa'
+import EmailIcon from '@mui/icons-material/Email';
+import styled from 'styled-components'
 
 const Splash = () => {
     return (
-        <div>
-            Splash Page
-        </div>
+        <FormModal open>
+            <Wrapper>
+                <h2>Welcome!</h2>
+                <Button
+                    variant='contained'
+                    startIcon={<FaUserSecret />}
+                >
+                    Continue As Guest
+                </Button>
+                <Button
+                    variant='contained'
+                    color='warning'
+                    startIcon={<EmailIcon />}
+                >
+                    Sign In With Email
+                </Button>
+            </Wrapper>
+        </FormModal>
     )
 }
 
 export default Splash
+
+
+const Wrapper = styled.div`
+    display: grid;
+    grid-auto-flow: row;
+    text-align: center;
+    gap: 1rem;
+`
